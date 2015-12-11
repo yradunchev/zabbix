4089681a0dc9fc9554f90a7fa5294549e680ea4f
Yordan Radunchev <yordan@radunchev.com>
2015-12-11 21:30:54 +0200

    Update fscheck.sh

--- fscheck.sh
+++ fscheck.sh
@@ -15,15 +15,8 @@ while [ "x${FSROLIST[count]}" != "x" ]
 do
         FSROCHECK=`touch ${FSROLIST[count]}/test.txt 2> /dev/null && { rm ${FSROLIST[count]}/test.txt 2> /dev/null; echo "0"; } || echo "1"`
         FSROCHECKTOTAL=$(( $FSROCHECKTOTAL + $FSROCHECK ))
-        if [ "$FSROCHECK" == 1 ] && [ -d "${FSROLIST[count]}" ]; then
-                echo $CURDATE ${FSROLIST[count]} is read only. >> /tmp/fsrocheck.log
-        fi
         count=$(( $count + 1 ))
 done
-
-if [ "$FSROCHECKTOTAL" == 0 ]; then
-        echo $CURDATE all file systems are writeable. >> /tmp/fsrocheck.log
-fi
 
 #Send the fsrochecktotal to zabbix
 /usr/bin/zabbix_sender -c /etc/zabbix/zabbix_agentd.conf -s $HOSTNAME -k filesystem.ro.check -o $FSROCHECKTOTAL
